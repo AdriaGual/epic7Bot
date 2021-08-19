76b1057af8aa558686f520bea0c7f1a8a68543d0
AdriaGual <u1933571@campus.udg.edu>
2021-08-19 10:56:11 +0200

    update abyss

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import templates
 from stages import arena, open_app, sanctuary, summon, abyss, mail, reputation, guild, altar, hunt
-
+import sys
 from utils import android_connection, global_utils
 import subprocess
 import time
 android_connection.connect(0)
 
-arena.start_arena(5)
+if len(sys.argv) > 1:
+    print(sys.argv[1])
+    if sys.argv[1] == "-abyss":
+        print("AAA")
